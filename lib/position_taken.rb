a5f062455a94db39b3f01e0d83dcfd262f707ecf
Christopher Del Basso <ccdelbasso@gmail.com>
2021-04-14 17:07:34 -0400

    commit tree 984aab4f42e36487d0dc9cbe94f74cb3332aa6b9

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,28 @@
 # code your #position_taken? method here!
+# board array 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# user input to convert to array index
+# input = gets.chomp
+
+# def input_to_index(input)
+#     input.to_i - 1
+# end
+
+# indexed player move for baord and new array
+#index = input_to_index(input)
+
+# def position_taken?(baord, index)
+#     if board[] == " "
+#         print false
+#     end
+# end
+
+
+def position_taken?(board, index)
+    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+       return false 
+    else
+       return true
+    end
+ end
